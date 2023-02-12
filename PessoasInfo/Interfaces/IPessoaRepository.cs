01ba28d95aba2d789ac9ecd5e57eefb57bf8ae5c
JGMelon22 <jgvasconcellos22@gmail.com>
2023-02-12 15:52:12 -0300

    Desenvolvendo Interface p/ Detalhe

--- PessoasInfo/Interfaces/IPessoaRepository.cs
+++ PessoasInfo/Interfaces/IPessoaRepository.cs
@@ -7,6 +7,6 @@ public interface IPessoaRepository
     Task<PessoaCreateViewModel> AddPessoa(PessoaCreateViewModel pessoaCreateViewModel);
     Task<IEnumerable<PessoaDetailViewModel>> GetPessoas();
     Task<PessoaDetailViewModel> GetPessoa(int id);
-    Task<PessoaEditViewModel> UpdatePessoa(int id, PessoaDetailViewModel pessoaDetailViewModel);
+    Task<PessoaEditViewModel> UpdatePessoa(int id, PessoaEditViewModel pessoaEditViewModel);
     Task DeletePessoa(int id);
 }
