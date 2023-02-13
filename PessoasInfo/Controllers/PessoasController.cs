8a6e3de2c3b8a041dfb2e6dca5909928abbb8e4d
JGMelon22 <jgvasconcellos22@gmail.com>
2023-02-13 19:54:52 -0300

    Desenvolvedo os métodos HTTPGET e HTTPOST p/a Delete na PessoasController

--- PessoasInfo/Controllers/PessoasController.cs
+++ PessoasInfo/Controllers/PessoasController.cs
@@ -1,3 +1,4 @@
+using DocumentFormat.OpenXml.Drawing;
 using PessoasInfo.Interfaces;
 
 namespace PessoasInfo.Controllers;
@@ -19,5 +20,34 @@ public class PessoasController : Controller
         return pessoas != null
             ? await Task.Run(() => View(pessoas))
             : await Task.Run(NoContent);
+    }
+
+    // Delete Pessoa
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var pessoa = await _pessoaRepository.GetPessoa(id);
+
+        if (id == null || pessoa == null)
+            return await Task.Run(NotFound);
+
+        var pessoaToRemove = await _pessoaRepository.GetPessoa(id);
+
+        return await Task.Run(() => View(pessoaToRemove));
+    }
+
+    [HttpPost]
+    [ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var pessoa = await _pessoaRepository.GetPessoa(id);
+
+        if (id == null || pessoa == null)
+            return await Task.Run(NotFound);
+
+        await _pessoaRepository.DeletePessoa(id);
+
+        return await Task.Run(() => RedirectToAction(nameof(Index)));
     }
 }
