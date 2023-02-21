50e70b91c70dd642184c8c5921b50ccf55b63334
JGMelon22 <jgvasconcellos22@gmail.com>
2023-02-21 11:12:07 -0300

    Desenvolvendo a Controller p/ Grafico

---
+++ PessoasInfo/Controllers/GraficosController.cs
@@ -0,0 +1,21 @@
+using PessoasInfo.Services;
+
+namespace PessoasInfo.Controllers;
+
+public class GraficosController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public GraficosController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> SimpleGraph()
+    {
+        var graphService = new GraphService(_context);
+        var graph = await graphService.GenerateGraph();
+
+        return await Task.Run(() => View(graph));
+    }
+}
