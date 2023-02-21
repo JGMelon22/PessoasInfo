3336accde6992b757debdb50a23567df83068552
JGMelon22 <jgvasconcellos22@gmail.com>
2023-02-21 11:05:01 -0300

    Desenvolvendo a ViewModel do relatório e service para criação do mesmo

---
+++ PessoasInfo/Services/GraphService.cs
@@ -0,0 +1,37 @@
+using PessoasInfo.ViewModels.Grafico;
+
+namespace PessoasInfo.Services;
+
+public class GraphService
+{
+    private readonly AppDbContext _context;
+
+    public GraphService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<GraficoViewModel>> GenerateGraph()
+    {
+        var lstModel = new List<GraficoViewModel>();
+        lstModel.Add(new GraficoViewModel()
+        {
+            DimensionOne = "Pessoas",
+            Quantity = await _context.Pessoas.CountAsync()
+        });
+
+        lstModel.Add(new GraficoViewModel()
+        {
+            DimensionOne = "Telefones",
+            Quantity = await _context.Telefones.CountAsync()
+        });
+
+        lstModel.Add(new GraficoViewModel()
+        {
+            DimensionOne = "Detalhes",
+            Quantity = await _context.Detalhes.CountAsync()
+        });
+
+        return lstModel;
+    }
+}
---
+++ PessoasInfo/ViewModels/Grafico/GraficoViewModel.cs
@@ -0,0 +1,7 @@
+namespace PessoasInfo.ViewModels.Grafico;
+
+public class GraficoViewModel
+{
+    public string? DimensionOne { get; set; }
+    public int Quantity { get; set; }
+}
