2ad534efd4d2c2aa35173588f527004ec2eb68ef
JGMelon22 <jgvasconcellos22@gmail.com>
2023-04-06 17:26:39 -0300

    Removendo chamada de views não existentes previamente usadas para testar Ploicies e claims

--- PessoasInfo/Controllers/AccessesController.cs
+++ PessoasInfo/Controllers/AccessesController.cs
@@ -9,26 +9,6 @@ public class AccessesController : Controller
         return await Task.Run(View);
     }
 
-    // Por cargo
-    [Authorize(Roles = "Basico")]
-    public IActionResult BasicAccess()
-    {
-        return View();
-    }
-
-    // Por cargo
-    [Authorize(Roles = "Comum")]
-    public IActionResult NormalAccess()
-    {
-        return View();
-    }
-
-    [Authorize(Roles = "Comum, Admin")]
-    public IActionResult AdminAndComumAccess()
-    {
-        return View();
-    }
-
     // Por política - Verifica se o usuário é admin ou não
     [Authorize(Policy = "OnlyAdminChecker")]
     public IActionResult OnlyAdminChecker()
